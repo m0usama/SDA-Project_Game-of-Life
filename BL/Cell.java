3bb11ef629961f55dc865df618a1d4f85ceb9071
Muhammad Usama <80623627+m0usama@users.noreply.github.com>
2021-11-27 13:32:50 +0500

    Added Nieghboir Property

--- BL/Cell.java
+++ BL/Cell.java
@@ -1,9 +1,22 @@
 public class Cell {
     int status;
-
+    int neighbours;
+    public Cell()
+    {
+        status=0;
+        neighbours=0;
+    }
     public int GetStatus()
     {
         return status;
+    }
+    public int GetNeighbour()
+    {
+        return neighbours;
+    }
+    public void SetNeighbour(int n)
+    {
+        this.neighbours=n;
     }
     public void SetStatus(int stat)
     {
