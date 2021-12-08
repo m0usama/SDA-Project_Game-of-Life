7eb6f739b86c88ed81a01eba45f401ab214ab5bc
Raida-Munir <94188433+Raida-Munir@users.noreply.github.com>
2021-12-08 19:24:03 +0500

    Delete forDB.java

--- BL/forDB.java
+++
@@ -1,6 +0,0 @@
-public interface ForDB {
-    public String[][] loadState (String StateName);
-    public void saveState();
-    public void deleteState(String StateName);
-    public int[][] viewState();
-}
