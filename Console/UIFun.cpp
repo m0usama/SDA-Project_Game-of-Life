8b9d136c793158a4bb8e04623b75203b2d5728c8
Sallahudin-Awan <94189793+Sallahudin-Awan@users.noreply.github.com>
2021-12-11 17:05:38 +0500

    Update UIFun.cpp

--- Console/UIFun.cpp
+++ Console/UIFun.cpp
@@ -1,11 +1,18 @@
 package UI;
-
-import com.company.GameOfLife;
+import BL.ForUI;
+import BL.GameOfLife;
 import java.io.IOException;
 import java.util.Scanner;
+import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
-import java.util.*;
+import java.awt.event.KeyAdapter;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+
 public class UIFun {
+
+    /*------------------------Printing of Grid-------------------*/
     public static void printing(int[][] array,int width,int length){
         System.out.print("\t");
         for(int i=0;i<width;i++){
@@ -22,90 +29,128 @@ public class UIFun {
                     }
                 }else{
                     if(j>9) {
-                        System.out.print("x  ");
+                        System.out.print("$  ");
                     }
                     else {
-                        System.out.print("x ");
+                        System.out.print("$ ");
                     }
                 }
             }
             System.out.println();
         }
-
-
     }
-    
+
+    /*Interrupt for Stop*/
+    static class MyKeyListener extends KeyAdapter {
+        public int m;
+        public void keyPressed(KeyEvent evt) {
+            if (evt.getKeyChar() == 'a') {
+                m=1;
+            }
+            if (evt.getKeyChar() == 'b') {
+                m=0;
+            }
+        }
+    }
     public static void main(String[] args) throws IOException, InterruptedException {
 
-
-       GameOfLife Game= new GameOfLife();
+        //GameOfLife Game= new GameOfLife();
+        ForUI Game=new GameOfLife();
 
         int length=10;
         int width=10;
         int[][] array = new int[10000][10000];
-       
-
         for (int i=0;i<10000;i++) {
             for(int j=0;j<10000;j++) {
                 array[i][j]=0;
-        }
+            }
         }
         array=Game.GetGridToUI();
         int option=-1,loop=0;
+
+        /*Loop Until Game Ends*/
         while(loop!=-1) {
 
+            /*-----------------Call Print Function After Every Iteration----------*/
             System.out.println(System.lineSeparator().repeat(100));
             printing(array, width, length);
-            //option Input
 
+            /*----------USer Can See this Menu and Enter Some Value-------------*/
+           
             System.out.println("0-Change Grid");
             System.out.println("1-Save State");
-            System.out.println("2-Load States");
+            System.out.println("2-Load Save States");
             System.out.println("3-Start");
             System.out.println("4-Zoom Level");
             System.out.println("5-Reset");
             System.out.println("6-Next State");
-            // create an object of Scanner
+            System.out.println("7-Speed Control");
+            System.out.println("8-Delete Save State");
+            System.out.print("Counter: ");
+            System.out.println(Game.getCounter());
+            System.out.println();
+
+
+            // take input that what Option User want to Choose
             Scanner input = new Scanner(System.in);
-            // take input from the user
             option = input.nextInt();
 
             if(option==0){
-                // create an object of Scanner
+                // Enter X Value
                 System.out.print("Enter X value: ");
                 Scanner input1 = new Scanner(System.in);
-                // take input from the user
                 int x = input1.nextInt();
+                // Enter X Value
                 System.out.print("Enter Y value: ");
                 Scanner input2 = new Scanner(System.in);
-                // take input from the user
                 int y = input2.nextInt();
+                /*Call BL Layer Function to Just pass x and y Coordinates to Make changes in Grid*/
                   Game.ManuallyDrawPattren(x,y);
-                array=Game.GetGridToUI();
+                  array=Game.GetGridToUI();
             }
             if(option==1) {
                 //Save Current State
-
+               Game.saveState();
             }
             if(option==2){
                 //Load State
+                System.out.println("Enter State Number : ");
+                Scanner input1 = new Scanner(System.in);
+                int State = input1.nextInt();
+                Game.LoadState(State);
+                array=Game.GetGridToUI();
             }
             if(option==3){
                 //Start State
+                JTextField component = new JTextField();
+                MyKeyListener My=new MyKeyListener();
+                component.addKeyListener(My);
+
+                JFrame f = new JFrame();
+
+                f.add(component);
+                f.setSize(300, 300);
+                f.setVisible(true);
                 int Loop1=-1;
+                if(Game.getCounter()==0){
+                Game.Start(1);
+                }
                 while(Loop1==-1){
                     Game.Next(0);
                     array=Game.GetGridToUI();
                     System.out.println(System.lineSeparator().repeat(100));
                     printing(array, width, length);
-                    TimeUnit.SECONDS.sleep(5);
+                    int m = My.m;
+                    if(m==1){
+                        Loop1=1;
+                    }
+                    TimeUnit.SECONDS.sleep(Game.getSpeed());
                 }
             }
             if(option==4){
-                // create an object of Scanner
+                // User Can Select Level From 1 to 4
                 System.out.print("Enter Level From 1 to 4: ");
                 Scanner input1 = new Scanner(System.in);
-                // take input from the user
                 int x = input1.nextInt();
                 if(x==1){
                     length=10;
@@ -121,13 +166,43 @@ public class UIFun {
                     width=40;
                 }
             } if(option==5){
+                //Reset the State
                 Game.Clear(0);
                 array=Game.GetGridToUI();
             }
             if(option==6){
-                //Next State
-                    Game.Next(0);
-                    array=Game.GetGridToUI();
+                    //Next State
+                if(Game.getCounter()==0){
+                    Game.Start(1);
+                }
+                Game.Next(0);
+                array=Game.GetGridToUI();
+            }
+            if(option==7){
+                // User Can Select Level From 1 to 4
+                System.out.print("Enter Speed From 1 to 4: ");
+                Scanner input1 = new Scanner(System.in);
+                int x = input1.nextInt();
+                if(x==1){
+                    Game.setSpeed(5);
+                }if(x==2){
+                    Game.setSpeed(4);
+                }if(x==3){
+                    Game.setSpeed(3);
+                }if(x==4){
+                    Game.setSpeed(2);
+                }
+            }
+            if(option==8) {
+                //Delete Save State
+                Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
+                System.out.print("Enter State Name: ");
+                String S= sc.nextLine(); //reads string.
+                Game.Deletestate(S);
+            }
+            if(option==9){
+
+
             }
         }
         
